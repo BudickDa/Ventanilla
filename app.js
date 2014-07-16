04a19ed7761d191caba1737c08884eab354b1115
budickda <daniel@budick.eu>
2014-07-16 19:08:15 +0200

    bugfixing at Logic

--- app.js
+++ app.js
@@ -45,31 +45,6 @@ app.get('/backend', function(req, res){
   res.render('backend');
 });
 
-app.post('updateBlock', function(req, res){
-  try{
-    //Block Object contains all the data we need
-    var block = req.body.block;
-  }
-  catch(e){
-    console.log(e);
-    return res.json(e);
-  }
-  try{
-    if(blocks[block.uid]===block){
-      console.log("Nothing new at "+block.uid);
-    }else{
-      console.log("Syncing Block " + block.uid);
-      delete blocks[block.uid];
-      ventanilla.registerBlock(block);
-    }
-  }catch(e){
-    console.log(e);
-    return res.json(e);
-  }
-  return res.json(200,true);
-});
-
-
 
 app.post('/api', function(req, res){
     var key = req.body.key;
